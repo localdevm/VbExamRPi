8bfd29270351d0dfe03d856b98f1db1cec23f46a
themaawaa <maartenwachtersmast@gmail.com>
2017-06-21 02:27:09 +0200

    dd

--- test.py
+++ test.py
@@ -6,13 +6,22 @@ IO.setup(19, IO.OUT)
 IO.setup(26, IO.IN, pull_up_down=IO.PUD_UP)
 
 IO.add_event_detect(26, IO.FALLING, bouncetime=200)
+IO.output(19,0)
 
+led1 = False
 def main():
     try:
         while True:
             if IO.event_detected(26):
-                print('event triggered')
-                IO.output(19,1)
+                if led1 == False:
+                    print('event triggered')
+                    IO.output(19,1)
+                    led1 = True
+
+                elif led1 == True:
+                    print('event triggered')
+                    IO.output(19,0)
+                    led1 = False
 
     except KeyboardInterrupt:
          print("Shutting down program and disabling pins")
