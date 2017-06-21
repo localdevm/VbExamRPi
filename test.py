1de571d4fc2296dd9331062ec10e35b1ff4fc260
themaawaa <maartenwachtersmast@gmail.com>
2017-06-21 02:07:09 +0200

    new test

---
+++ test.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as IO
+import time
+IO.setmode(IO.BCM)
+
+IO.setup(19, IO.OUT)
+IO.setup(26, IO.IN, pull_up_down=IO.PUD_UP)
+
+IO.add_event_detect(26, IO.FALLING, bouncetime=200)
+
+def main():
+    try:
+        while True:
+            if IO.event_detected(26):
+                print('event triggered')
+
+                 #do nothing
+
+        except KeyboardInterrupt:
+            print('stopped')
+            pass
+        finally:
+            IO.cleanup()
+
+if __name__ == "__main__":
+    main()
